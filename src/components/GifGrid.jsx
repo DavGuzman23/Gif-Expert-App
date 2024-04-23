7785bd7b9194ca2c64e0a21a4161c8322014f79f
DavGuzman23 <gummer2310@gmail.com>
2024-04-23 16:11:50 +0200

    ➕ Show title images for screen

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react"
 import { getGifs } from "../helpers/getDifs"
 
 export const GifGrid = ( { category } ) => {
 
-  getGifs(category)
+  const [images, setImages] = useState([])
 
+  // maneja todos los efectos, si el array final no se pone nada es para que se renderice solo la primera vez al lanzarlo
+  useEffect( () => {
+    getGifs(category)
+      .then(newImages => setImages(newImages))
+  }, [])
 
+  
   return (
     <>
       <h3> {category} </h3>
+
+      <ol>
+        {
+          images.map( ({id, title})=> (
+            <li key={id}> {title} </li>
+          ))
+        }
+      </ol>
     </>
   )
 }
